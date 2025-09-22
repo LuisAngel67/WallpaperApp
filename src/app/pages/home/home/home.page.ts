f13f3b58169d9e17adec367c45d95cec3e18fd72
Luis Zabaleta <luisangelzabaleta67@gmail.com>
2025-09-22 02:21:53 -0500

    Le di funcionalidad a la pagina de actualizar perfil , traje los datos para los campos, hice el metodo para actualizar el documento, todo eso usando getdoc, updatedoc y otros metodos de firestore , tambien use el onauthstate para que se carguen los datos siempre asi se recargue la pagina y le implemente una pequeña funcionalidad al home para que aparezca el nombre del usuario que esta logueado

--- src/app/pages/home/home/home.page.ts
+++ src/app/pages/home/home/home.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Auth as AuthFirebase,
+  onAuthStateChanged,
+  User,
+} from '@angular/fire/auth';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +13,20 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class HomePage implements OnInit {
-  constructor() {}
+  userName: string = '';
 
-  ngOnInit() {}
+  constructor(private auth: AuthFirebase, private firestore: Firestore) {}
+
+  ngOnInit() {
+    onAuthStateChanged(this.auth, async (user: User | null) => {
+      if (user) {
+        const userDoc = doc(this.firestore, 'users', user.uid);
+        const userSnap = await getDoc(userDoc);
+        if (userSnap.exists()) {
+          const data = userSnap.data();
+          this.userName = data['name'] || '';
+        }
+      }
+    });
+  }
 }
--- src/app/pages/update/update/update.page.ts
+++ src/app/pages/update/update/update.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import {
+  Auth as AuthFirebase,
+  onAuthStateChanged,
+  User,
+} from '@angular/fire/auth';
+import { Firestore, doc, getDoc, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-update',
@@ -10,17 +16,47 @@ import { Router } from '@angular/router';
 })
 export class UpdatePage implements OnInit {
   UpdateForm!: FormGroup;
+  uid: string = '';
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private auth: AuthFirebase,
+    private firestore: Firestore
+  ) {}
 
   ngOnInit() {
     this.UpdateForm = this.fb.group({
       name: ['', [Validators.required]],
       lastName: ['', Validators.required],
     });
+
+    onAuthStateChanged(this.auth, async (user: User | null) => {
+      if (user) {
+        this.uid = user.uid;
+        const userDoc = doc(this.firestore, 'users', this.uid);
+        const userSnap = await getDoc(userDoc);
+        if (userSnap.exists()) {
+          const data = userSnap.data();
+          this.UpdateForm.patchValue({
+            name: data['name'] || '',
+            lastName: data['lastName'] || '',
+          });
+        }
+      }
+    });
   }
 
-  update() {}
+  async update() {
+    if (this.uid) {
+      const userDoc = doc(this.firestore, 'users', this.uid);
+      await updateDoc(userDoc, {
+        name: this.UpdateForm.value.name,
+        lastName: this.UpdateForm.value.lastName,
+      });
+    }
+    this.router.navigate(['/home']);
+  }
 
   cancelUpdate() {
     this.router.navigate(['/home']);
