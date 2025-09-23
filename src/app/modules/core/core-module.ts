38320f7d35134135f451b9ba997ae0abfb039a62
Luis Zabaleta <luisangelzabaleta67@gmail.com>
2025-09-22 23:34:21 -0500

    Instale las librerias necesarias para poder usar translate en la app , tambien cree la carpeta i18n con los archivos en y es .json para manejar las traducciones , tambien reconfigure el coremodule

--- src/app/modules/core/core-module.ts
+++ src/app/modules/core/core-module.ts
@@ -8,17 +8,45 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { Query } from './providers/query/query';
 import { ToastService } from './providers/toast/toast';
 import { Uploader } from './providers/uploader/uploader';
+import { saveImageService } from './providers/saveImage/save-image';
+import { AddedImage } from './providers/addedImage/added-image';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-const providers = [Auth, Query, ToastService, Uploader];
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+const providers = [
+  Auth,
+  Query,
+  ToastService,
+  Uploader,
+  saveImageService,
+  AddedImage,
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'en',
+    }),
+  ],
   providers: [
     provideFirebaseApp(() => initializeApp(environment.FIREBASE_CONFIG)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     providers,
   ],
+  exports: [TranslateModule],
 })
 export class CoreModule {}
