1cd28d63f59a6fa29d56e45e70d099684d749e8f
Luis Zabaleta <luisangelzabaleta67@gmail.com>
2025-09-23 12:51:35 -0500

    Ya probe la app en android y fuinciona bien , ahora en el card component agregue el actionsheetcontroller para que cuando se clickee una imagen , salgan las opciones abajo de lo que se quiere , solo falta implemetar el plugin y hacerle ciertos ajustes para finzalizar

--- src/app/modules/shared/shared/components/card/card.component.ts
+++ src/app/modules/shared/shared/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +11,42 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CardComponent implements OnInit {
   @Input() imageUrl: string = '';
 
-  constructor() {}
+  constructor(
+    private actionSheetCtrl: ActionSheetController,
+    private translate: TranslateService
+  ) {}
+
+  async onImageClick() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: this.translate.instant('ACTIONSHEET.HEADER'),
+      buttons: [
+        {
+          text: this.translate.instant('ACTIONSHEET.WALLPAPER'),
+          icon: 'image',
+          handler: () => {
+            console.log('Poner como fondo de pantalla:', this.imageUrl);
+            // Aquí usare al plugin cuando lo haga
+          },
+        },
+        {
+          text: this.translate.instant('ACTIONSHEET.LOCKSCREEN'),
+          icon: 'lock-closed',
+          handler: () => {
+            console.log('Poner en pantalla de bloqueo:', this.imageUrl);
+            // Aquí tambien usare al plugin
+          },
+        },
+        {
+          text: this.translate.instant('ACTIONSHEET.CANCEL'),
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {},
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 
   ngOnInit() {}
 }
